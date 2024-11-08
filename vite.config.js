be9378294d39a1a8acc5223e74c2ea2b4c52e4b7
salmanulfarisvnb <salmanulfarisp9946@gmail.com>
2024-11-08 14:35:10 +0530

    some issues fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/redux_crud_app>/",
+  base: "/redux_crud_app/",
   plugins: [react()],
 });
